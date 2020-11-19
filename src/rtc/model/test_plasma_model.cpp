b12c4539085fc5f952b80af3613389fe4a54257e
rikuto-yasuda <yasuda.r@pparc.gp.tohoku.ac.jp>
2020-11-19 16:27:34 +0900

    add test plasma and magnet model

---
+++ src/rtc/model/test_plasma_model.cpp
@@ -0,0 +1,38 @@
+// basic_plasma_model.cpp: basic_plasma_model クラスのインプリメンテーション
+//
+//////////////////////////////////////////////////////////////////////
+#include "StdAfx.h"
+#include "test_model.h"
+
+using namespace rtc;
+using namespace model;
+
+// test_null model ////////////////////////////////////////////////////////
+double plasma::test_null_plasma::getDensity( const vector& point ) const
+{
+	return 0;
+}
+
+// test_simple model //////////////////////////////////////////////////////
+double plasma::test_simple::getDensity( const vector& point ) const
+{
+	const double Re = getCosmos().getPlanet().getRadius();
+
+	vector pa = point, pb = point;
+	pa(0) -= 3*Re;
+	pb(0) += 3*Re;
+
+	const double
+		r = std::fabs( 
+			  (point(0)*point(0) - Re*Re/2)
+			+ (point(1)*point(1) - Re*Re/2)
+			+ (point(2)*point(2) - Re*Re/2)
+		);
+
+	return 5e14*(
+		Re/r
+		+ 1.5*Re/(inner_prod(pa,pa))
+		+ 1.5*Re/(inner_prod(pb,pb))
+	);
+}
+
