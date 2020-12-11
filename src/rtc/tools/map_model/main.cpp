a4d1b33f57d44832de0fc9241f657d4fa4e3ff93
rikuto-yasuda <yasuda.r@pparc.gp.tohoku.ac.jp>
2020-12-11 11:47:19 +0900

    fix
    fix tools

--- src/rtc/tools/map_model/main.cpp
+++ src/rtc/tools/map_model/main.cpp
@@ -1,12 +1,12 @@
 #include "main.h"
 int plot_plasma( rtc::basic_plasma_model& p )
 {
-	std::clog << "plotting plasma-xy";
-	plot_plasma_H("pxy_normal", p, 0 );
+//	std::clog << "plotting plasma-xy";
+//	plot_plasma_H("pxy_normal", p, 0 );
 
 	std::clog << "plotting plasma-xz";
 	plot_plasma_V("pxz-normal", p, 0 );
-
+/*
 	const double Re = p.getMother().getRadius();
 	p.addCavity( rtc::cavity(
 		0.3,
@@ -15,8 +15,9 @@ int plot_plasma( rtc::basic_plasma_model& p )
 		7.0*Re,
         0.8*Re
 	));
-	std::clog << "plotting plasma-xy";
-    plot_plasma_H("pxy_cav", p, 0 );
+*/
+//	std::clog << "plotting plasma-xy";
+//    plot_plasma_H("pxy_cav", p, 0 );
 
 	std::clog << "plotting plasma-xz";
     plot_plasma_V("pxz-cav", p, 0 );
@@ -24,19 +25,22 @@ int plot_plasma( rtc::basic_plasma_model& p )
 	return 0;
 }
 
+/*
 int plot_magnet( rtc::basic_magnet_model& m )
 {
 	plot_magnet("m0", m, 0, 55);
 	return 0;
 }
+*/
 
 int main()
 {
-	rtc::cosmos c( 1980,5,6, 0,0,0 );
-	rtc::model::magnet::IGRF       m(4);
-	rtc::model::plasma::sato_earth p;
-	rtc::planet::earth e(m,p);
-	const double Re = e.getRadius();
+//	rtc::cosmos c( 1980,5,6, 0,0,0 );
+//	rtc::model::magnet::IGRF       m(4);
+//	rtc::model::plasma::sato_earth p;
+//	rtc::planet::earth e(m,p);
+	rtc::model::plasma::test_simple p;
+//	const double Re = e.getRadius();
 	/*p.addCavity( rtc::cavity(
 		0.03,
 		60,10,
@@ -44,7 +48,7 @@ int main()
 		7.0*Re,
 		0.8*Re
 		));*/
-	c.registerPlanet(e);
+//	c.registerPlanet(e);
 
 	//plot_magnet(m);
 	plot_plasma(p);
